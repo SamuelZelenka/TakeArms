78eb2d6481862254077a3914044ce8ced08ad417
Samuel Zelenka <samuel.zelenka@hotmail.com>
2023-03-05 00:20:34 +0100

    Updated unit repository namespace

--- Assets/Scripts/Repository/UnitRepository.cs
+++ Assets/Scripts/Repository/UnitRepository.cs
@@ -3,7 +3,7 @@ using TakeArms.Configurations;
 
 using UnityEngine;
 
-namespace TakeArms.Targeting
+namespace TakeArms.Repository
 {
     [Serializable]
     [CreateAssetMenu(fileName = "new UnitRepository", menuName = "Repository/Unit Repository", order = 0)]
